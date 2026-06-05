6d7973149b39aa14a46e055b88acdce925fe183d
Valelearn1 <valentina-ferro@hotmail.it>
2026-06-05 23:04:52 +0200

    fix layout tablet and desktop

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,14 @@
 //
 // Aggiungi qui il codice JavaScript se serve (es. modale, scroll handler).
 // Per la Versione Base spesso non serve niente: tutto si fa in CSS.
+
+const logo = document.getElementById("logo");
+const heroContent = document.getElementById("heroContent");
+
+logo.addEventListener("click", (e) => {
+  heroContent.classList.remove("animate");
+
+  void heroContent.offsetWidth; // forziamo il browser a registrare la rimozione per poterla aggiungere ogni volta che si clicca il logo
+
+  heroContent.classList.add("animate");
+});
